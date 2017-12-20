15523583e275b57d3bb50e2a5fca6b6622e3970d
Jared Chu <me@jaredchu.com>
2017-12-20 19:15:14 +0700

    .

--- app/SalesSequenceMeta.php
+++ app/SalesSequenceMeta.php
@@ -9,4 +9,5 @@ class SalesSequenceMeta extends Model
     protected $table = 'sales_sequence_meta';
     protected $guarded = [];
     protected $primaryKey = 'meta_id';
+    public $timestamps = false;
 }
